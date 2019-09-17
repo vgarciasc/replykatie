eb604d1bc93b2c268610b89bff53ac9939d57ced
vinigarcia <vingarciasc@gmail.com>
2019-09-16 21:59:48 -0300

    initial commit

---
+++ Assets/Scripts/GameCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class GameCamera : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
