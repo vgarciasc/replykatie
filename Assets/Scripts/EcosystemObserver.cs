bde3f4be94c7d97f4b69f3ea7c5277d3fc372461
vinigarcia <vingarciasc@gmail.com>
2019-10-07 20:27:23 -0300

    adding TMPro, adding population counter in UI

---
+++ Assets/Scripts/EcosystemObserver.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class EcosystemObserver : MonoBehaviour
+{
+    public static EcosystemObserver GetEcosystemObserver() {
+        return (EcosystemObserver) HushPuppy.safeFindComponent("GameController", "EcosystemObserver");
+    }
+
+    [SerializeField]
+    TextMeshProUGUI rabbitCounter;
+    [SerializeField]
+    TextMeshProUGUI flowerCounter;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        rabbitCounter.text = GameObject.FindObjectsOfType<Creature>().Length.ToString();
+        flowerCounter.text = GameObject.FindObjectsOfType<Plant>().Length.ToString();
+    }
+}
